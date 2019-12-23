31115a7469668eba4c59386b05559203dcd5861d
SASAGAWA, Kiyoshi <sasagawa@kent-and-co.com>
2019-12-23 22:04:30 +0900

    タイトルとmeta descriptionが上手く読み込めていないようなので決め打ち (#2)

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,45 +1,44 @@
-
 export default {
   mode: 'spa',
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'WordCamp Osaka 2019ひとつき遅れのアドベントカレンダー',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'WordCamp Osaka 2019の実行委員による"WordCamp Osaka 2019のつくりかた"にかんするリレーブログの案内'
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
   /*
-  ** Customize the progress-bar color
-  */
+   ** Customize the progress-bar color
+   */
   loading: { color: '#fff' },
   /*
-  ** Global CSS
-  */
-  css: [
-    { src: '~/assets/sass/common.scss', lang: 'scss' }
-  ],
+   ** Global CSS
+   */
+  css: [{ src: '~/assets/sass/common.scss', lang: 'scss' }],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
   /*
-  ** Nuxt.js dev-modules
-  */
+   ** Nuxt.js dev-modules
+   */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module'
   ],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://github.com/nuxt-community/modules/tree/master/packages/bulma
     '@nuxtjs/bulma',
@@ -50,9 +49,9 @@ export default {
     '@nuxtjs/dotenv'
   ],
   /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
   axios: {
     proxy: true
   },
@@ -60,8 +59,8 @@ export default {
     '/api': { target: 'https://wco2019.unplat.info' }
   },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     postcss: {
       preset: {
@@ -71,9 +70,8 @@ export default {
       }
     },
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend (config, ctx) {}
   }
 }
